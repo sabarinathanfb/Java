0dfa740b024f8790584c9177849d7e871e75dda8
sabarinathanfb <77018757+sabarinathanfb@users.noreply.github.com>
2024-05-14 15:16:49 +0530

    Demo for Comparable and Comparator

---
+++ java/src/ComparableAndComparator/Employee.java
@@ -0,0 +1,61 @@
+package ComparableAndComparator;
+
+import java.util.Comparator;
+
+public class Employee implements Comparable<Employee>, Comparator<Employee> {
+    private int id;
+    private String name;
+    private int age;
+    private long salary;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public long getSalary() {
+        return salary;
+    }
+
+
+    public Employee(int id, String name, int age, int salary) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    @Override
+    public int compareTo(Employee emp){
+
+        return (this.age - emp.age);
+
+    }
+
+
+    @Override
+    //this is overridden to print the user-friendly information about the Employee
+    public String toString() {
+        return "[id=" + this.id + ", name=" + this.name + ", age=" + this.age + ", salary=" +
+                this.salary + "]";
+    }
+    public static Comparator<Employee> SalaryComparator = new Comparator<Employee>() {
+
+        @Override
+        public int compare(Employee e1, Employee e2) {
+            return (int) (e1.getSalary() - e2.getSalary());
+        }
+    };
+
+    @Override
+    public int compare(Employee o1, Employee o2) {
+        return o1.getAge() - o2.getAge();
+    }
+}
